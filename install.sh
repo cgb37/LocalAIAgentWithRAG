b05d61d5b5d0b3332527f5d641147dedda433ead
Charles <cgb37@miami.edu>
2025-04-14 14:38:20 -0400

    feat: add installation script template
    Creates a new shell script to serve as a template for installation tasks.
    
    This template includes basic structure and an example echo command, allowing for easy customization and expansion for future installation logic.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script Name: install
+# Description: TODO: Add description
+# Author: charlesbrownroberts
+# Date Created: 4/14/25
+# Last Modified: 4/14/25
+
+# Main code starts here
+echo "Hello, this is a script template!"
+
+# Add your script's logic here
+
